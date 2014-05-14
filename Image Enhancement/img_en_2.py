0a2994d32621b8c77ee6339e355360c60cd7ca75
Vinícius Petch <vinicius.ap21@gmail.com>
2014-05-13 22:02:20 -0300

    Melhoramento nas minutiaes, tentativa de core

--- Image Enhancement/img_en_2.py
+++ Image Enhancement/img_en_2.py
@@ -35,9 +35,7 @@ def searchPath(img1, img2, x, y):
 			return False
     return True
 
-
-    
-s = '102_6'
+s = '102_1'
 
 # Image input
 img = cv2.imread(s + '.tif',0)
@@ -112,10 +110,13 @@ for i in range(1, height-1):
 	    imgrgb[i+1, j] = [0, 255, 0]
 	    imgrgb[i+1, j+1] = [0, 255, 0]
 
+# Minutiaes 1: bifurcacoes, ou seja, pontos que possuem 3 pontos adjacentes
+# Minutiaes 2: finais de linha, ou seja, que possuem apenas 1 ponto adjacente
 cv2.imwrite(s + "_mn_1_2.bmp", imgrgb)
 cv2.imwrite(s + "_mn_1_only.bmp", ctrl1)
 cv2.imwrite(s + "_mn_2_only.bmp", ctrl2)
 
+# Borda falsa - para evitar falha de segmentacao e variantes
 for i in range(0, height):
     img[i, 0] = 0
     img[i, width-1] = 0
@@ -125,6 +126,22 @@ for i in range(0, width):
 
 ctrl3 = ctrl1.copy()
 
+# Retiradas de "bifurcacoes" unidas
+for i in range(1, height-1):
+    for j in range(1, width-1):
+	if ctrl3[i, j] == 255:
+	    ctrl3[i-1, j-1] = 0
+	    ctrl3[i-1, j] = 0
+	    ctrl3[i-1, j+1] = 0
+	    ctrl3[i, j-1] = 0
+	    ctrl3[i, j+1] = 0
+	    ctrl3[i+1, j-1] = 0
+	    ctrl3[i+1, j] = 0
+	    ctrl3[i+1, j+1] = 0
+
+cv2.imwrite(s + "_mn_1_clean.bmp", ctrl3)
+
+# Remocao de bifurcacoes falsas
 for i in range(1, height-1):
     for j in range(1, width-1):
 	if ctrl1[i, j] == 255:
@@ -139,11 +156,21 @@ for i in range(1, height-1):
 	    if count < 2:
 		ctrl3[i, j] = 0
 
+# Minutiaes escolhidas
 cv2.imwrite(s + "_mn_1_selection.bmp", ctrl3)
-cv2.imwrite(s + "_mn_1_selection2.bmp", ctrl1)
+
+for i in range(0, height):
+    for j in range(0, width):
+	if img[i, j] == 255:
+	    if ctrl3[i, j] == 0:
+		ctrl3[i, j] = 40
+
+# Minutiaes com a digital no fundo
+cv2.imwrite(s + "_mn_1_selection2.bmp", ctrl3)
 
 ctrl3 = ctrl2.copy()
 
+# Inicios e terminos de linha
 for i in range(1, height-1):
     for j in range(1, width-1):
 	if ctrl2[i, j] == 255:
@@ -158,5 +185,36 @@ for i in range(1, height-1):
 	    if count >= 1:
 		ctrl3[i, j] = 0
 
+# Minutiaes escolhidas
 cv2.imwrite(s + "_mn_2_selection.bmp", ctrl3)
-cv2.imwrite(s + "_mn_2_selection2.bmp", ctrl2)
+
+for i in range(0, height):
+    for j in range(0, width):
+	if img[i, j] == 255:
+	    if ctrl3[i, j] == 0:
+		ctrl3[i, j] = 40
+
+# Minutiaes com a digital no fundo
+cv2.imwrite(s + "_mn_2_selection2.bmp", ctrl3)
+
+img = cv2.imread(s + "_mn_1_selection.bmp", 0)
+tX = 0
+tY = 0
+qt = 0
+for i in range(0, height):
+    for j in range(0, width):
+	if img[i, j] == 255:
+	    tX += i
+	    tY += j
+	    qt += 1
+
+img = np.zeros(img.shape,np.uint8)
+tX /= qt
+tY /= qt
+
+for i in range(-1, 2):
+    for j in range(-1, 2):
+	img[tX+i, tY+j] = 255
+
+
+cv2.imwrite(s + "_core.bmp", img)
